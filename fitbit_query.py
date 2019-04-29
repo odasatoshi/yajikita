c482b9394c01b026d596573359c4541605546225
ODA Satoshi <oda.satoshi@gmail.com>
2019-04-29 22:14:21 +0900

    Add user profile.
    - ユーザプロファイルとしてdisplaynameとAvatarを追加して管理するようにした。
    - ユーザがupdateしても動作するようにした。

--- fitbit_query.py
+++ fitbit_query.py
@@ -28,6 +28,26 @@ def regist(acode):
         user_id = ret["user_id"]
         access_token = ret["access_token"]
         refresh_token = ret["refresh_token"]
-        user_master.update_user(user_id, access_token, refresh_token)
+        user_master.update_user(user_id, access_token=access_token, refresh_token=refresh_token)
     else:
         print("ERROR")
+
+def get_user_profile(uname, access_token):
+    auth = 'Bearer ' + access_token
+    headers = {
+        'Authorization': auth,
+    }
+    response = requests.get('https://api.fitbit.com/1/user/'+ uname + '/profile.json', headers=headers)
+    if response.status_code == 200:
+        ret = json.loads(response.text)["user"]
+        avatar = ret["avatar150"]
+        displayName = ret["displayName"]
+        user_master.update_user(uname, displayName=displayName, avatar=avatar)
+    else:
+        print("ERROR")
+
+# updated / 1h 
+def renew_per_hour():
+    users = user_master.list_user()
+    for s_user in users:
+        get_user_profile(s_user[0],s_user[1])
--- user_master.py
+++ user_master.py
@@ -9,20 +9,49 @@ def initialize():
     try:
         cursor.execute("DROP TABLE IF EXISTS USER")
         cursor.execute(
-    "CREATE TABLE IF NOT EXISTS USER (userid TEXT PRIMARY KEY, access_token TEXT, refresh_token TEXT)")
+    "CREATE TABLE IF NOT EXISTS USER (userid TEXT PRIMARY KEY, access_token TEXT, refresh_token TEXT, displayName TEXT, avatar TEXT)")
     except sqlite3.Error as e:
         print('sqlite3.Error occurred:', e.args[0])
     connection.commit()
     connection.close()
 
-def update_user(uname, access_token, refresh_token=None):
+def update_user(uname, access_token="", refresh_token="", displayName="", avatar=""):
     connection = sqlite3.connect(user_db)
     cursor = connection.cursor()
     try:
-        cursor.execute(
-    "INSERT INTO USER VALUES (?, ? , ?)",  (uname,access_token,refresh_token))
+        cursor.execute('SELECT * FROM USER WHERE userid=?', (uname,))
+        data = cursor.fetchall()
+        if len(data) == 0:
+            cursor.execute(
+            "INSERT INTO USER VALUES (?, ?, ?, ?, ?)",  (uname,access_token,refresh_token, displayName, avatar))
+        else:
+            if access_token == "":
+                cursor.execute(
+                "UPDATE USER SET displayName=?, avatar=? WHERE userid=?",
+                  (displayName, avatar, uname))
+            else:
+                cursor.execute(
+                "UPDATE USER SET access_token=?, refresh_token=? WHERE userid=?",
+                  (access_token,refresh_token, uname))
+
     except sqlite3.Error as e:
         print('sqlite3.Error occurred:', e.args[0])
     connection.commit()
     connection.close()
 
+def list_user():
+    connection = sqlite3.connect(user_db)
+    cursor = connection.cursor()
+    try:
+        result = cursor.execute(
+    "SELECT userid, access_token FROM USER")
+        data = cursor.fetchall()
+    except sqlite3.Error as e:
+        print('sqlite3.Error occurred:', e.args[0])
+    connection.commit()
+    connection.close()
+    return data
+
+    
+
+    
